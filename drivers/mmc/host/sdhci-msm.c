078b740e9399d4ebea529e5f367fe1f11b460c3b
Leonid Segal <leonid.s@variscite.com>
2018-11-08 17:04:20 +0200

    Revert "mmc: sdhci-msm: Fix DLL phase selection"
    This reverts commit 28f13d7f112a4e1fa67f0996e4d115748206d68b.

--- drivers/mmc/host/sdhci-msm.c
+++ drivers/mmc/host/sdhci-msm.c
@@ -270,7 +270,7 @@ out:
  * setting for SD3.0 UHS-I card read operation (in SDR104
  * timing mode) or for eMMC4.5 card read operation (in
  * HS400/HS200 timing mode).
- * Select the median value of the range and configure the DLL with the
+ * Select the 3/4 of the range and configure the DLL with the
  * selected DLL clock output phase.
  */
 
@@ -361,7 +361,9 @@ static int msm_find_most_appropriate_phase(struct sdhci_host *host,
 		}
 	}
 
-	i = curr_max / 2;
+	i = (curr_max * 3) / 4;
+	if (i)
+		i--;
 
 	ret = ranges[selected_row_index][i];
 
